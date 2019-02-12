d74b9deee842f39c88bba5bf7ad0275f4b018280
Stefan Urosevic <stefanuros@gmail.com>
2019-02-11 20:32:37 -0500

    Player Movement
    Working on the player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private bool canJump;
+	public int jumpForce;
+	public int moveSpeed;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		canJump = true;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		//If up was presed
+		if(Input.GetKey("up"))
+		{
+			//Jump
+			if(canJump)
+			{
+				canJump = false;
+				GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+			}
+
+		}
+
+		//Adding left and right movement
+		if(Input.GetKey("right"))
+		{
+			//Adding constraints to the movement
+			if(transform.position.x < 9)
+			{
+				transform.position += (new Vector3(1, 0, 0) * moveSpeed);
+			}
+		}
+		if(Input.GetKey("left"))
+		{
+			//Adding constraints to the movement
+			if(transform.position.x > -9)
+			{
+				transform.position += (new Vector3(-1, 0, 0) * moveSpeed);
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Ground")
+		{
+			canJump = true;
+		}
+	}
+}
