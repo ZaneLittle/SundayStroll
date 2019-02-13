ed60f3af1669f5549ec71da1ce928260e17b6200
Stefan Urosevic <stefanuros@gmail.com>
2019-02-13 00:06:34 -0500

    Added Parallax
    Added Parallax to the background
    
    close #26

---
+++ Assets/Scripts/ParallaxMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxMovement : MonoBehaviour
+{
+	private float speed;
+	public int layer;
+
+	void Start()
+	{
+		Parallax p = this.gameObject.GetComponentInParent<Parallax>();
+		
+		speed = (p.speed - (layer * p.speed/10.0f)*p.relateSpeed);
+	}
+
+	void Update()
+	{
+		transform.position = new Vector3(transform.position.x-speed, transform.position.y, transform.position.z);
+		if(transform.position.x < -8.85f*2)
+		{
+			transform.position = new Vector3(8.85f*2, transform.position.y, transform.position.z);
+		}
+	}
+}
