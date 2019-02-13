dae755f8ab3e307cf64ef9ede05e984b49ef8813
Zane Little <zanetlittle@gmail.com>
2019-02-12 22:05:20 -0500

    feat: Add wait timer for spawn generation
    - add configurable wait timer
    - reconfigure definition of obstacle Y values
    
    Close #24

--- Assets/Scripts/ObstacleSpawners.cs
+++ Assets/Scripts/ObstacleSpawners.cs
@@ -4,49 +4,40 @@ using UnityEngine;
 
 public class ObstacleSpawners : MonoBehaviour
 {
-    private float x = 11.0f;
-    private float headY = -3.65f;    // Height for the obstacle at head height
-    private float groundY = -2.3f;   // Height for ground obstacle
-    private float holeY = -5.0f;     // Height for hole obstacle
+    private float x;
+    private float[] y;
 
     public Transform parent;
     public Transform obstaclePrefab;
-    public int inverseDifficulty = 500;
-
-    /**
-     * Spawns an obstacle and returns the index of the obstacle type:
-     * 0 = no obstacle spawned
-     * 1 = head height obstacle
-     * 2 = ground height obstacle
-     * 3 = hole obstacle
-     */
-    int spawn()
-    {
-        Vector3 position;
-        int obstacleNum = Random.Range(1, inverseDifficulty + 4);
-        switch (obstacleNum)
+    public int inverseSpawnRate;    // Denominator of spawn probability
+    public float buffer;            // Min time between spawns
+    
+     IEnumerator spawn()
+     {
+        while (true)
         {
-            case 1:
-                position = new Vector3(x, headY, 0);
-                break;
-            case 2:
-                position = new Vector3(x, groundY, 0);
-                break;
-            case 3:
-                position = new Vector3(x, holeY, 0);
-                break;
-            default:
-                return 0; // No obstacle spawned
-        }
-        Transform obstacle = Instantiate(obstaclePrefab, position, Quaternion.identity);
-        obstacle.parent = parent;
-        return obstacleNum;
-    }
 
-    // Update is called once per frame
-    void Update()
+            int obstacleNum = Random.Range(0, inverseSpawnRate + 3);
+            if (obstacleNum < 3)
+            {
+                Vector3 position = new Vector3(x, y[obstacleNum], 0);
+                Transform obstacle = Instantiate(obstaclePrefab, position, Quaternion.identity);
+                obstacle.parent = parent;
+                yield return new WaitForSeconds(buffer);
+            }
+        }
+     }
+
+    void Start()
     {
-        // TODO: only spawn overy x seconds
-        spawn();
+        x = 11.0f;
+        y = new float[3] {
+            -3.65f, // Head height obstacle
+            -2.3f,  // Ground height obstacle
+            -5.0f   // Hole obstacle
+        };
+        inverseSpawnRate = 10;
+        buffer = 3.0f;
+        StartCoroutine(spawn());
     }
 }
