7090e981a333acdf0734f1219db4d088b6fecdb6
Zane Little <zanetlittle@gmail.com>
2019-02-20 21:32:12 -0500

    feat: Add restart on R keypress
    - Reset game when R is pressed
    - Convert reset game function to be asynchronous
    
    Close #61

--- Assets/Scripts/RestartManager.cs
+++ Assets/Scripts/RestartManager.cs
@@ -6,17 +6,39 @@ using UnityEngine.SceneManagement;
 
 public class RestartManager : MonoBehaviour
 {
+    private bool transitioning;
+
     public Button restartButton;
 
-    private void resetGame()
+    private IEnumerator resetScene()
     {
+        transitioning = true; // Lock corountine from multiple instances
         Debug.Log("Resetting scene.");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        AsyncOperation resetAsync = SceneManager
+            .LoadSceneAsync(
+                SceneManager.GetActiveScene().name
+            );
+
+        yield return resetAsync;
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && !transitioning)
+        {
+            StartCoroutine(resetScene());
+        }
     }
 
     private void Start()
     {
+        transitioning = false;
         restartButton.interactable = false;
-        restartButton.onClick.AddListener(() => resetGame());
+        restartButton.onClick.AddListener(() =>
+        {
+            StartCoroutine(resetScene());
+        });
     }
 }
